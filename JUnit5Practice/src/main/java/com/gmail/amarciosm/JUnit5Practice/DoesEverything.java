54959c4619cb60b8023c16f0550fdcd890b6ae04
Márcio Martins <a.marcio.sm@gmail.com>
2022-09-17 14:28:33 -0300

    JUnit 5 ready

--- JUnit5Practice/src/main/java/com/gmail/amarciosm/JUnit5Practice/DoesEverything.java
+++ JUnit5Practice/src/main/java/com/gmail/amarciosm/JUnit5Practice/DoesEverything.java
@@ -1,9 +1,21 @@
 package com.gmail.amarciosm.JUnit5Practice;
 
+import java.util.List;
+
 public class DoesEverything {
 
 	public static void main(String[] args) {
 		System.out.println("Hello JUnit 5");
+	}
+	
+	public Integer sum(List<Integer> integers) {
+		Integer total = 0;
+		
+		for(Integer i : integers) {
+			total += i;
+		}
+		
+		return total;
 	}
 	
 }
---
+++ JUnit5Practice/src/test/java/com/gmail/amarciosm/JUnit5Practice/DoesEverythingTest.java
@@ -0,0 +1,14 @@
+package com.gmail.amarciosm.JUnit5Practice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DoesEverythingTest {
+
+	@Test
+	void testSum() {
+		fail("Not yet implemented");
+	}
+
+}
